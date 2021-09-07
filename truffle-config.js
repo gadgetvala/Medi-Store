144850cecd2ff4bf93d2a099952f98675d03fdf5
gadgetvala <surajj.verma7@gmail.com>
2021-09-07 10:51:28 +0530

    REFACTOR: Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  networks: {},
-  mocha: {},
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+  },
+  contracts_directory: "./contracts/",
+  contracts_build_directory: "./build/",
   compilers: {
     solc: {
       version: "0.4.24",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
   },
 };
