ca89df43e0994067239160160324c615fea01f3c
gadgetvala <surajj.verma7@gmail.com>
2021-09-08 09:42:38 +0530

    FEATURE: Added Doctor Screen

---
+++ src/screens/DoctorScreen/DoctorScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoctorScreen = () => {
+  return (
+    <div className="doctorScreen">
+      <h1>Hello from Doctor Screen</h1>
+    </div>
+  );
+};
+
+export default DoctorScreen;
