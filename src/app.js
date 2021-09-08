d9739b5de5833cf4c1361f7ff4a508e43e7ce132
gadgetvala <surajj.verma7@gmail.com>
2021-09-08 09:38:48 +0530

    FEATURE: Mount Pages

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Main from "./../Router/MainAppRouter";
+import RolesScreen from "./screens/RolesScreen/RolesScreen";
+import DoctorScreen from "./screens/DoctorScreen/DoctorScreen";
+import PatientScreen from "./screens/PatientScreen/PatientScreen";
+import RegisterPatientScreen from "./screens/RegisterPatientScreen/RegisterPatientScreen";
 
 const App = (props) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Main} exact />
+        <Route path="/" component={RolesScreen} exact />
+        <Route path="/doctor" component={DoctorScreen} exact />
+        <Route path="/patient" component={PatientScreen} exact />
+        <Route
+          path="/register-patient"
+          component={RegisterPatientScreen}
+          exact
+        />
       </Switch>
     </BrowserRouter>
   );
