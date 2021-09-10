6bd91cb85f765c1341c4d3fe391a44181e7da2af
gadgetvala <surajj.verma7@gmail.com>
2021-09-10 10:54:16 +0530

    FEATURE: Mount NotificationTost

--- src/app.js
+++ src/app.js
@@ -1,24 +1,31 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import RolesScreen from "./screens/RolesScreen/RolesScreen";
-import DoctorScreen from "./screens/DoctorScreen/DoctorScreen";
-import PatientScreen from "./screens/PatientScreen/PatientScreen";
-import RegisterPatientScreen from "./screens/RegisterPatientScreen/RegisterPatientScreen";
+import RolesScreen from "screens/RolesScreen/RolesScreen";
+import DoctorScreen from "screens/DoctorScreen/DoctorScreen";
+import PatientScreen from "screens/PatientScreen/PatientScreen";
+import RegisterPatientScreen from "screens/RegisterScreen/RegisterScreen";
+import { AppProvider } from "context/AppContext";
+import NotificationToast from "components/notificationToast/NotificationToast";
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={RolesScreen} exact />
-        <Route path="/doctor" component={DoctorScreen} exact />
-        <Route path="/patient" component={PatientScreen} exact />
-        <Route
-          path="/register-patient"
-          component={RegisterPatientScreen}
-          exact
-        />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <AppProvider>
+        <NotificationToast />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={RolesScreen} exact />
+            <Route path="/doctor" component={DoctorScreen} exact />
+            <Route path="/patient" component={PatientScreen} exact />
+            <Route
+              path="/register/:slug"
+              component={RegisterPatientScreen}
+              exact
+            />
+          </Switch>
+        </BrowserRouter>
+      </AppProvider>
+    </>
   );
 };
 
