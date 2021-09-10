5b75334aba13d03069cfecfafef2ca24bfdae09b
gadgetvala <surajj.verma7@gmail.com>
2021-09-10 10:54:47 +0530

    FEATURE: Added Check is user exist then redirect to they role based screens

--- src/screens/RolesScreen/RolesScreen.js
+++ src/screens/RolesScreen/RolesScreen.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { Redirect } from "react-router-dom";
 import { Link } from "react-router-dom";
+import mediStore from "web3_config/medistore";
+import { AppContext } from "context/AppContext";
+import ClipLoader from "react-spinners/BounceLoader";
 import "./styles.css";
 
 const RolesScreen = () => {
+  const { user, setUser, setNotificationTostValue } = useContext(AppContext);
+  const [loading, setLoading] = useState(true);
+
+  const checkUserExitOrNot = async () => {
+    try {
+      const userData = await mediStore.methods.getUserData().call();
+      setUser({ ...userData });
+      if (userData.role !== "")
+        setNotificationTostValue("Details Fetch Successfully");
+      setLoading(false);
+    } catch (err) {
+      setNotificationTostValue("Error While Fetching User Data");
+    }
+  };
+
+  useEffect(() => {
+    checkUserExitOrNot();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="rolesScreen">
+        <ClipLoader color={"#22577A"} size={80} />
+      </div>
+    );
+  }
+
+  if (user.role !== "" && user.role === "Patient") {
+    return <Redirect to="/patient" />;
+  }
+
+  if (user.role !== "" && user.role === "Doctor") {
+    return <Redirect to="/doctor" />;
+  }
+
   return (
     <div className="rolesScreen">
-      <Link to="/doctor">Doctor</Link>
-      <Link to="/patient">Patient</Link>
+      <Link to="/register/Doctor">Doctor</Link>
+      <Link to="/register/Patient">Patient</Link>
     </div>
   );
 };
