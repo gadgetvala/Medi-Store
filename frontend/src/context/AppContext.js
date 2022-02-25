11daf7b7432ae16daa101f5376049abe8bb8ff37
gadgetvala <surajj.verma7@gmail.com>
2022-02-25 15:04:25 +0530

    REFACTOR: Remove Old Toast Configuration

--- frontend/src/context/AppContext.js
+++ frontend/src/context/AppContext.js
@@ -4,16 +4,12 @@ export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [user, setUser] = useState({});
-  const [notificationToastValue, setNotificationTostValue] = useState("");
 
-  console.log(user);
   return (
     <AppContext.Provider
       value={{
         user,
         setUser,
-        notificationToastValue,
-        setNotificationTostValue,
       }}
     >
       {children}
