079c9799da2986566331088dcb683976e96f1e1f
gadgetvala <surajj.verma7@gmail.com>
2022-02-25 14:59:54 +0530

    FEATURE: Added New Function for Toast

---
+++ frontend/src/components/notificationToast/ShowToast.js
@@ -0,0 +1,13 @@
+import { toast } from "react-toastify";
+
+export default (message) => {
+  return toast(message, {
+    position: "top-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
