f0b53497e44cdd70161c92c23caf92f13287bb03
gadgetvala <surajj.verma7@gmail.com>
2022-02-25 14:59:40 +0530

    REFACTOR: Update Show Notification Function

--- frontend/src/components/notificationToast/NotificationToast.js
+++ frontend/src/components/notificationToast/NotificationToast.js
@@ -1,24 +1,8 @@
-import React, { useEffect, useContext } from "react";
-import { AppContext } from "context/AppContext";
-import { ToastContainer, toast } from "react-toastify";
+import React from "react";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const NotificationToast = () => {
-  const { notificationToastValue } = useContext(AppContext);
-
-  useEffect(() => {
-    if (notificationToastValue !== "")
-      toast(notificationToastValue, {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-  });
-
   return (
     <div>
       <ToastContainer />
